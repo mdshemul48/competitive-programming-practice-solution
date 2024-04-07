943ef7657de8d8d3a20521672c12cf0e517d6239
MD. Shimul <mdshemul480@gmail.com>
2024-04-07 23:01:47 +0600

    adding week-3 problem

---
+++ week-4/day-3/Alt_Tab.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// custom define
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+#define endl '\n'
+#define ll long long
+#define pub push_back
+#define pob pop_back
+#define all(X) (X).begin(), (X).end()
+
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << -1 << endl
+#define cout(v) cout << v << endl;
+#define arrin(type, arr, n)     \
+    type arr[n];                \
+    for (int i = 0; i < n; i++) \
+    cin >> arr[i]
+
+void exc()
+{
+    int n;
+    cin >> n;
+    arrin(string, arr, n);
+
+    map<string, bool> mp;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (mp.find(arr[i]) == mp.end())
+        {
+            cout << arr[i][arr[i].size() - 2] << arr[i][arr[i].size() - 1]; 
+        }
+        mp[arr[i]] = 1;
+    }
+    cout << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    exc();
+    return 0;
+}
