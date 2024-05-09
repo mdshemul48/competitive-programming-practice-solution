ea0ad71cad39261269d0cee66251d637f19d78a9
MD. Shimul <mdshemul480@gmail.com>
2024-05-09 22:45:47 +0600

    adding new problem

---
+++ week-7/day-7/B_Array_Cancellation.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// custom define
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+#define endl '\n'
+#define int long long
+#define pub push_back
+#define pob pop_back
+#define all(X) (X).begin(), (X).end()
+
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << -1 << endl
+#define cout(v) cout << v << endl;
+#define arrin(type,arr,n) type arr[n]; for (int i =0; i<n; i++) cin>>arr[i]
+
+
+
+void exc()
+{
+    int t,n;
+    cin>>t;
+    while(t--){
+       cin>>n; 
+
+        int pos = 0, neg = 0; 
+        for (int i = 0; i<n; i++){
+            int x; 
+            cin>>x; 
+            if (x>=0) pos+= x; 
+            else {
+                if (pos+x>=0) pos+=x; 
+                else {
+                    x+=pos; 
+                    pos = 0; 
+                    neg+= x; 
+                }
+            }
+        }
+        cout<< -neg<<endl;
+    }
+
+    
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    exc();
+    return 0;
+}
